e106d0918134f4899c95fa1e316239ae875598e1
nicowesse <nico.wesse@gmail.com>
2022-02-22 20:34:59 +0100

    Added ofxClipper-libs

---
+++ src/ofxCortex/utils/Graphics.cpp
@@ -0,0 +1,8 @@
+//
+//  Graphics.cpp
+//  cortex
+//
+//  Created by Nicolay Wesseltoft on 2/22/22.
+//
+
+#include "Graphics.hpp"
---
+++ src/ofxCortex/utils/Graphics.h
@@ -0,0 +1,13 @@
+//
+//  Graphics.hpp
+//  cortex
+//
+//  Created by Nicolay Wesseltoft on 2/22/22.
+//
+
+#ifndef Graphics_hpp
+#define Graphics_hpp
+
+#include <stdio.h>
+
+#endif /* Graphics_hpp */
