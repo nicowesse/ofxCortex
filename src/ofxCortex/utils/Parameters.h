3845f9b96dd8058b6b2a715b7c9942d2fda7e269
Nicolay Wesseltoft <nico.wesse@gmail.com>
2022-04-22 17:20:52 +0200

    Added new Box-type. Similar to ofRectangle

---
+++ src/ofxCortex/utils/Parameters.h
@@ -0,0 +1,13 @@
+//
+//  ParameterUtils.hpp
+//  oba-colonizer
+//
+//  Created by Nicolay Wesseltoft on 12/04/2022.
+//
+
+#ifndef ParameterUtils_hpp
+#define ParameterUtils_hpp
+
+#include <stdio.h>
+
+#endif /* ParameterUtils_hpp */
