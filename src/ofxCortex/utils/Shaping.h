7ac840f9b1f6046e0fef41897ddd8af7e683b233
nicowesse <nico.wesse@gmail.com>
2022-05-15 22:34:11 +0200

    Added a biaed gain-function

--- src/ofxCortex/utils/Shaping.h
+++ src/ofxCortex/utils/Shaping.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace ofxCortex { namespace core {
+namespace ofxCortex { namespace core { namespace utils {
 
 class Shaping {
 public:
@@ -11,10 +11,22 @@ public:
     return (x < 0.5) ? a : 1.0 - a;
   }
   
+  static float biasedGain(float x, float amplitude = 2.0f, float bias = 0.5f)
+  {
+    if (x < 0.0f || x > 1.0f)
+    {
+      ofLogVerbose("Shaping::gain") << "\n\tThe x value needs to be between 0.0 and 1.0. Returning " << x;
+      return x;
+    }
+    
+    //bias = pow((1.0 - bias) + 0.5, 6);
+    return pow(x, amplitude) / (pow(x, amplitude) + pow(bias - bias * x, amplitude));
+  };
+  
   static float parabola( float x, float k )
   {
       return pow( 4.0*x*(1.0-x), k );
   }
 };
 
-}}
+}}}
