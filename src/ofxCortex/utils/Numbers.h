d0424fd03ab5f267afc1e9fd39e5b4545b5f1f19
Nicolay Wesseltoft <nico.wesse@gmail.com>
2024-01-25 17:05:48 +0100

    Adding Lerped<T>

--- src/ofxCortex/utils/Numbers.h
+++ src/ofxCortex/utils/Numbers.h
@@ -28,4 +28,47 @@ static unsigned long modulo(int a, int b) { return (b + (a % b)) % b; }
 template<typename T>
 static double normalizeIndex(size_t index, const std::vector<T> & v) { return (double) index / (v.size() - 1); }
 
+template<typename T = float>
+class Lerped {
+public:
+  Lerped(T value = T(), float smoothing = 0.1f, const std::string & name = "Lerped Value") : current(value), target(value)
+  {
+    ofAddListener(ofEvents().update, this, &Lerped::update);
+    
+    parameters.setName(name);
+    parameters.add(currentValue, this->smoothing);
+    this->smoothing.set(smoothing);
+  }
+  
+  ~Lerped()
+  {
+    ofRemoveListener(ofEvents().update, this, &Lerped::update);
+  }
+  
+  void operator=(const T& value) { this->setTarget(value); }
+  operator T() const { return current; }
+  
+  void setTarget(const T & value) { this->target = value; }
+  void setCurrent(const T & value) { this->current = value; }
+  void setSmoothing(float value) { this->smoothing = value; }
+  
+  
+  ofParameterGroup parameters;
+protected:
+  T current;
+  T target;
+  
+  ofParameter<std::string> currentValue { "Value", "0.0" };
+  ofParameter<float> smoothing { "Smoothing", 0.1, 0.0, 1.0 };
+  
+  void update(ofEventArgs & e) { current = ofLerp(current, target, 1.0 - pow(this->smoothing.get(), ofGetLastFrameTime())); currentValue = ofToString(current); }
+};
+
+template<>
+inline void Lerped<glm::vec3>::update(ofEventArgs & e)
+{
+  float t = 1.0 - pow(this->smoothing.get(), ofGetLastFrameTime());
+  current = glm::mix(current, target, t);
+}
+
 };
